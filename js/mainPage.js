2b455c5054c02082ffc9812d465db2460b1fcc4a
Octavio Lima <octaviolimasan@gmail.com>
2022-11-01 02:25:31 -0300

    Atualização da tela de prestadores, possível cadastrar e suspender prestadores

--- js/mainPage.js
+++ js/mainPage.js
@@ -3,7 +3,10 @@ const workContainerPage = document.querySelectorAll(".work-container-page");
 const pageWindow = document.querySelectorAll(".page-window");
 const blockMainPage = document.querySelector(".block-main-page");
 
+const employeeTable = document.querySelector(".employee-table");
+const addEmployeeButton = document.querySelector(".add-employee-button");
 
+// Funções
 function clickOnSidebarButton(clicked, buttonID) {
     sidebarButtons.forEach((button, index) => {
         button.classList.remove("selected");
@@ -34,7 +37,7 @@ document.addEventListener("click", (e) => {
     e.preventDefault();
 
     const targetEl = e.target;
-    const parentEl = targetEl.closest("div");
+    const parentEl = targetEl.closest("tr");
 
     if (targetEl.classList.contains("sidebar-button")){
         let targetId = targetEl.id;
@@ -52,4 +55,62 @@ document.addEventListener("click", (e) => {
         openWindow(targetId);
     }
 
+    if (targetEl.classList.contains("emp-pause-btn")){
+        parentEl.classList.toggle("suspended-emp");
+    }
+    
+    if (targetEl.classList.contains("emp-remove-btn")){
+        parentEl.remove();
+    }
+})
+
+/* Adicionar Prestador */
+
+function addEmployee(name, acessType, loginCred) {
+    const newEmployee = document.createElement("tr");
+    newEmployee.classList.add("em-table-row");
+
+    const employeeName = document.createElement("td");
+    employeeName.innerText = name;
+    newEmployee.appendChild(employeeName);
+    
+    const employeeAcessType = document.createElement("td");
+    employeeAcessType.innerText = acessType;
+    newEmployee.appendChild(employeeAcessType);
+
+    const employeeLoginCred = document.createElement("td");
+    employeeLoginCred.innerText = loginCred;
+    newEmployee.appendChild(employeeLoginCred);
+
+    const tableDataBtnEdit = document.createElement("td")
+    const empEditBtn = document.createElement("button");
+    empEditBtn.classList.add("taskbar-buttons");
+    empEditBtn.classList.add("emp-edit-btn");
+    empEditBtn.innerHTML = ' <i class="fa fa-pencil"></i>';
+    tableDataBtnEdit.appendChild(empEditBtn)
+    newEmployee.appendChild(tableDataBtnEdit);
+
+    const tableDataBtnPause = document.createElement("td")
+    const empPauseBtn = document.createElement("button");
+    empPauseBtn.classList.add("taskbar-buttons");
+    empPauseBtn.classList.add("emp-pause-btn");
+    empPauseBtn.innerHTML = ' <i class="fa fa-pause"></i>';
+    tableDataBtnPause.appendChild(empPauseBtn)
+    newEmployee.appendChild(tableDataBtnPause);
+
+    const tableDataBtnRemove = document.createElement("td")
+    const empRemoveBtn = document.createElement("button");
+    empRemoveBtn.classList.add("taskbar-buttons");
+    empRemoveBtn.classList.add("emp-remove-btn");
+    empRemoveBtn.innerHTML = ' <i class="fa fa-xmark"></i>';
+    tableDataBtnRemove.appendChild(empRemoveBtn)
+    newEmployee.appendChild(tableDataBtnRemove);
+
+    employeeTable.appendChild(newEmployee);
+}
+
+addEmployeeButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    addEmployee("abc", "nope", "123");
 })
